63f39c12ddc79dec24c841b0948250aa056b858d
ZjCantarona <74037702+ZjCantarona@users.noreply.github.com>
2023-06-25 21:42:51 +0800

    Update views.py

--- church/views.py
+++ church/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 from users.models import UserProfile
 
 @login_required
 def index(request):
+    return HttpResponse('<h1>Django Include URLs</h1>')
     template = "index.html"
     try:
         profile = UserProfile.objects.get_or_create(user=request.user)
