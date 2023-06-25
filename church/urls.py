f2f863f14db9048a3257244dd0a4001bbf382c06
ZjCantarona <74037702+ZjCantarona@users.noreply.github.com>
2023-06-25 20:28:37 +0800

    Update urls.py

--- church/urls.py
+++ church/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
+from django.conf.urls.static import serve
 
 from .views import index
 
